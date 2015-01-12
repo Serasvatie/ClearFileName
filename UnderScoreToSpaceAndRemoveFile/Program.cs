d2ef8ea6520773b8b653ab490f175d11ed0f93cb
kilua5472 <quentin.favre@epitech.eu>
2015-01-12 19:03:11 +0100

    Version 1.0

---
+++ UnderScoreToSpaceAndRemoveFile/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderScoreToSpaceAndRemoveFile
+{
+    class Program
+    {
+        static void RuleFile(string file)
+        {
+            string extension = Path.GetExtension(file);
+            if (extension == ".url")
+                File.Delete(file);
+        }
+
+        static void RuleDirectory(string directory)
+        {
+            string newName = directory.Replace("_", " ");
+            if (newName != directory)
+                Directory.Move(directory, newName);
+        }
+
+        static void DirRecursive(string Dir)
+        {
+            foreach (string d in Directory.GetDirectories(Dir))
+            {
+                foreach (string f in Directory.GetFiles(d))
+                {
+                    RuleFile(f);
+                }
+                RuleDirectory(d);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                    throw new Exception("Bad Number Argument");
+                DirRecursive(args[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error : " + e.Message + "!");
+            }
+            Console.WriteLine("Success !");
+            Console.ReadLine();
+        }
+    }
+}
