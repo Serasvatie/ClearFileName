5678a1ddae4574fb2a4437bb17063b704f7fdfe4
kilua5472 <quentin.favre@epitech.eu>
2015-01-13 12:21:20 +0100

    Version 1.1 (Parsing Argument+Factory object)

---
+++ UnderScoreToSpaceAndRemoveFile/Factory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderScoreToSpaceAndRemoveFile
+{
+    class Factory
+    {
+        private Boolean Recursive = false;
+        private enum target { FILE = 1, DIRECTORY = 2, ALL = 0 };
+        private target Cible;
+        private string GeneralDiretory;
+
+        private void RemoveSpecificFile(string file)
+        {
+            string extension = Path.GetExtension(file);
+            if (extension == ".url")
+                File.Delete(file);
+        }
+
+        private void ChangeName(string directory)
+        {
+            string newName = directory.Replace("_", " ");
+            if (newName != directory)
+                Directory.Move(directory, newName);
+        }
+
+        public void DirRecursive()
+        {
+            foreach (string d in Directory.GetDirectories(GeneralDiretory))
+            {
+                foreach (string f in Directory.GetFiles(d))
+                {
+                    RemoveSpecificFile(f);
+                    if (Cible == target.ALL || Cible == target.FILE)
+                        ChangeName(f);
+                }
+                if (Cible == target.ALL || Cible == target.DIRECTORY)
+                    ChangeName(d);
+                if (Recursive)
+                    DirRecursive();
+            }
+        }
+
+        public void ParseArgument(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Substring(0, 2) == "--")
+                {
+                    if (arg == "--file")
+                        Cible = target.FILE;
+                    if (arg == "--directory")
+                        Cible = target.DIRECTORY;
+                    if (arg == "--all")
+                        Cible = target.ALL;
+                }
+                else if (arg == "-d")
+                    Recursive = true;
+                else
+                    GeneralDiretory = arg;
+            }
+        }
+    }
+}
