c6f39057e2121e38195803805fd56e386fd2655d
kilua5472 <quentin.favre@epitech.eu>
2015-01-14 20:22:25 +0100

    Version WPF V 1.0

---
+++ ClearFileNameWPF/Factory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearFileNameWPF
+{
+    class Factory
+    {
+        private Boolean Recursive = false;
+        public enum target { FILE = 1, DIRECTORY = 2, ALL = 0 };
+        private target Cible;
+        private string GeneralDiretory;
+
+        private void RemoveSpecificFile(string file)
+        {
+            string extension = Path.GetExtension(file);
+            if (extension == ".url")
+                File.Delete(file);
+        }
+
+        private void ChangeNameDirectory(string directory)
+        {
+            string newName = directory.Replace("_", " ");
+            if (newName != directory)
+                Directory.Move(directory, newName);
+        }
+
+        private void ChangeNameFile(string file)
+        {
+            int index = file.LastIndexOf("\\");
+            string newPath = file.Substring(0, index) + file.Substring(index).Replace("_", " ");
+            if (newPath != file)
+                File.Move(file, newPath);
+        }
+
+        private void DirRecursive(string defaultParam = "default")
+        {
+            foreach (string d in Directory.GetDirectories(defaultParam == "default" ? GeneralDiretory : defaultParam))
+            {
+                foreach (string f in Directory.GetFiles(d))
+                {
+                    if (Cible == target.ALL || Cible == target.FILE)
+                        ChangeNameFile(f);
+                    RemoveSpecificFile(f);
+                }
+                if (Recursive)
+                    DirRecursive(d);
+                if (Cible == target.ALL || Cible == target.DIRECTORY)
+                    ChangeNameDirectory(d);
+            }
+        }
+
+        public void InitAndDoIt(string path, Boolean rec, target cible)
+        {
+            GeneralDiretory = path;
+            Recursive = rec;
+            Cible = cible;
+            DirRecursive();
+        }
+    }
+}
---
+++ ClearFileNameWPF/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClearFileNameWPF
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _directory;
+        public string Directory
+        {
+            get
+            {
+                return _directory;
+            }
+            set
+            {
+                _directory = value;
+                this.OnPropertyChanged("Directory");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Directory = "Select Path...";
+            this.DataContext = this;
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            if (!FileBox.IsChecked.Value && !DirectoryBox.IsChecked.Value)
+                System.Windows.MessageBox.Show("Check File or Directory !");
+
+            Factory factory = new Factory();
+            Factory.target cible = new Factory.target();
+            if (FileBox.IsChecked.Value)
+            {
+                cible = Factory.target.FILE;
+                if (DirectoryBox.IsChecked.Value)
+                    cible = Factory.target.ALL;
+            }
+            if (DirectoryBox.IsChecked.Value)
+                cible = Factory.target.DIRECTORY;
+            factory.InitAndDoIt(Directory, RecursiveBox.IsChecked.Value, cible);
+        }
+
+        private void ChooseDirectory(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            Directory = dialog.SelectedPath;
+        }
+    }
+}
